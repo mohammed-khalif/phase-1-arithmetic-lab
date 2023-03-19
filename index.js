d222f5b2c0340bf760be7524fdd7218f3d1007e2
mohammed-khalif <mohamed.yusuf@student.moringaschool.com>
2023-03-18 23:00:39 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.ceil(Math.random() * 10);
+
+let num3 = 10;
+let num4 = 6;
+let mod = num3%num4;
+
+let nums = [2,4,6, 8, 10, 12, 14, 16, 18, 20];
+let max = Math.max.apply(null, nums);
+
+
+
+console.log(mode);
